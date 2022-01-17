1e415da873cf583960fe15e773383b19170e0f2b
dpudovkin <dpudovkin@sbrf.ru>
2022-01-17 19:01:00 +0300

    First commit

---
+++ src/main/java/edu/my/app/MyTodoListAppApplication.java
@@ -0,0 +1,13 @@
+package edu.my.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTodoListAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyTodoListAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/my/app/MyTodoListAppApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.my.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTodoListAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
